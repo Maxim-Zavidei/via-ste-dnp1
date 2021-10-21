ab2d81c8060bf678514a2b5cdf56343763f66433
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-10-21 23:48:34 +0200

    Solution for exercise 7, lecture 2

---
+++ Lecture2/IdentifyPalindromes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IdentifyPalindromes {
+    class Program {
+        static bool IsPalindrome(string x) {
+            for (int i = 0; i < x.Length; i++)
+                if (x[i] == ' ')
+                    x = x.Remove(i, 1);
+            for (int i = 0; i < x.Length / 2; i++)
+                if (x[i] != x[x.Length - i - 1])
+                    return false;
+            return true;
+        }
+
+        static void Main(string[] args) {
+            Console.WriteLine(IsPalindrome("murder for a jar of red rum"));
+        }
+    }
+}
