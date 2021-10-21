ea4837bbbceb9aa72882ec7a95a3d15aa5d0612c
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-10-21 23:57:22 +0200

    Solution for exercise 1, lecture 3

---
+++ Lecture3/JSONIntro/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace JSONIntro {
+    class Program {
+        static void Main(string[] args) {
+            Person p1 = new() {
+                FirstName = "1",
+                LastName = "2",
+                Age = 65,
+                Height = 35,
+                isMarried = true,
+                Sex = 'M',
+                Hobbies = new string[] {"coding", "skiing", "climbing"}
+            };
+            Person p2 = new() {
+                FirstName = "2",
+                LastName = "3",
+                Age = 42,
+                Height = 54,
+                isMarried = false,
+                Sex = 'F',
+                Hobbies = new string[] {"reading", "coding", "skiing", "climbing"}
+            };
+            Person p3 = new() {
+                FirstName = "3",
+                LastName = "4",
+                Age = 35,
+                Height = 12,
+                isMarried = true,
+                Sex = 'F',
+                Hobbies = new string[] {"reading", "coding", "skiing", "climbing"}
+            };
+            List<Person> People = new();
+            People.Add(p1);
+            People.Add(p2);
+            People.Add(p3);
+            string jsonFormatted = JsonSerializer.Serialize(People, new JsonSerializerOptions {WriteIndented = true});
+            Console.WriteLine(jsonFormatted);
+        }
+    }
+}
