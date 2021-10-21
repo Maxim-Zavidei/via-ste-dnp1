ae6292fcfd5090096038224561601c018b014d4b
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-10-21 23:19:17 +0200

    Solution for exercise 5, lecture 1

---
+++ Lecture1/MathInSeparateNamespace/MathLib/MathLib.cs
@@ -0,0 +1,15 @@
+﻿namespace MathInSeparateNamespace.MathLib {
+    class Calculator {
+        public int Add(int a, int b) {
+            return a + b;
+        }
+
+        public int Add(int[] arr) {
+            int sum = 0;
+            foreach (int i in arr) {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Lecture1/MathInSeparateNamespace/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MathInSeparateNamespace.MathLib;
+
+namespace MathInSeparateNamespace {
+    class Program {
+        static void Main(string[] args) {
+            var c = new Calculator();
+
+            Console.WriteLine(c.Add(4, 2));
+            Console.WriteLine(c.Add(new int[] {4, 8, 1, 3}));
+        }
+    }
+}
