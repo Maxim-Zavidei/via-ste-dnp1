5da4e24ea4716721ae2e129fc7c6642bb9032596
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-10-21 23:22:54 +0200

    Solution for exercise 6, lecture 1

---
+++ Lecture1/MoreMath/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MoreMath.MathLib;
+
+namespace MoreMath {
+    class Program {
+        static void Main(string[] args) {
+            var c = new Calculator();
+            c.Maximum(); 
+        }
+    }
+}
