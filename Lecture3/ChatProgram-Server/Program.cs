ef3bd5c9dc42627aa477ac3938b666b23e2c3423
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-10-22 00:13:01 +0200

    Solution for exercise 5, lecture 3

---
+++ Lecture3/ChatProgram-Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ChatProgram_Server {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Starting Server...");
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener listener = new TcpListener(ip, 5000);
+            listener.Start();
+
+            Console.WriteLine("Server started...");
+            var handler = new SocketHandlerClient();
+
+            while (true) {
+                Thread thread = new Thread(() => handler.HandleClient(listener.AcceptTcpClient()));
+                thread.Start();
+            }
+        }
+    }
+}
---
+++ Lecture3/ChatProgram-Server/SocketHandlerClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatProgram_Server {
+    class SocketHandlerClient {
+        public List<NetworkStream> connectedClients = new List<NetworkStream>();
+
+        public void HandleClient(TcpClient Client) {
+            NetworkStream stream = Client.GetStream();
+            connectedClients.Add(stream);
+
+            byte[] dataFromClient = new byte[1024];
+            while (true) {
+                int bytesRead = stream.Read(dataFromClient, 0, dataFromClient.Length);
+                string s = Encoding.ASCII.GetString(dataFromClient, 0, bytesRead);
+                if (s == "exit") break;
+                Console.WriteLine(s);
+
+                Broadcast(s);
+            }
+
+            connectedClients.Remove(stream);
+            Client.Close();
+        }
+
+        public void Broadcast(string message) {
+            foreach (var client in connectedClients) {
+                byte[] dataToClient = Encoding.ASCII.GetBytes($"Returning {message}");
+                client.Write(dataToClient, 0, dataToClient.Length);
+            }
+        }
+    }
+}
