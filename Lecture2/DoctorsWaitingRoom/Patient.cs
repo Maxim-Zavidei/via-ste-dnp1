93ac01685a65a35c381e4d38590e724e95b6895e
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-10-21 23:45:23 +0200

    Solution for exercise 4, lecture 2

---
+++ Lecture2/DoctorsWaitingRoom/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DoctorsWaitingRoom {
+    class Patient {
+        private int numberInQueue;
+        private WaitingRoom waitingRoom;
+
+        public Patient(WaitingRoom wr) {
+            numberInQueue = wr.DrawNumber();
+            waitingRoom = wr;
+            waitingRoom.NumberChange += this.ReactToNumber;
+        }
+
+        public void ReactToNumber(int ticketNumber) {
+            Console.WriteLine("Patient " + numberInQueue + " looks up");
+            if (ticketNumber == numberInQueue) {
+                Console.WriteLine("Patient with ticket number " + numberInQueue + " has entered the room.");
+                waitingRoom.NumberChange -= this.ReactToNumber;
+            }
+        }
+    }
+}
---
+++ Lecture2/DoctorsWaitingRoom/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DoctorsWaitingRoom {
+    class Program {
+        static void Main(string[] args) {
+            var wr = new WaitingRoom();
+            var p1 = new Patient(wr);
+            var p2 = new Patient(wr);
+            var p3 = new Patient(wr);
+
+            wr.RunWaitingRoom();
+        }
+    }
+}
