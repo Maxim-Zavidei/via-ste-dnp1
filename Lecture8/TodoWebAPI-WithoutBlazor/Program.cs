244f306e5d3b398f6c9ad0ebe3e8eec9238443f3
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-10-28 17:11:00 +0200

    Solution for exercise Todo Web API - without blazor from lecture 8

---
+++ Lecture8/TodoWebAPI-WithoutBlazor/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TodoWebAPI_WithoutBlazor {
+    class Program {
+        
+        private string uri = "http://jsonplaceholder.typicode.com";
+        private readonly HttpClient client;
+        
+        public Program() {
+            client = new HttpClient();
+        }
+        
+        public async Task<IList<Todo>> GetTodosAsync(int? userId, int? id, bool? isCompleted) {
+
+            string finalUri = $"{uri}/Todos?";
+            if (userId != null) finalUri += $"userId={userId}&";
+            if (id != null) finalUri += $"id={id}&";
+            if (isCompleted != null) finalUri += $"completed={isCompleted.ToString().ToLower()}&";
+
+            finalUri = finalUri.Substring(0, finalUri.Length - 1);
+            Console.WriteLine(finalUri);
+
+            HttpResponseMessage responseMessage = await client.GetAsync(finalUri);
+
+            if (!responseMessage.IsSuccessStatusCode)
+                throw new Exception($@"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+
+            string result = await responseMessage.Content.ReadAsStringAsync();
+
+            List<Todo> todos = JsonSerializer.Deserialize<List<Todo>>(result, new JsonSerializerOptions {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+
+            return todos;
+        }
+        
+        public async Task AddTodoAsync(Todo todo) {
+            string todoAsJson = JsonSerializer.Serialize(todo);
+
+            StringContent content = new StringContent(todoAsJson, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync(uri + "/Todos", content);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception($@"Error: {response.StatusCode}, {response.ReasonPhrase}");
+        }
+
+        public async Task RemoveTodoAsync(int todoId) {
+            HttpResponseMessage response = await client.DeleteAsync($"https://jsonplaceholder.typicode.com/Todos?id={todoId}");
+            if (!response.IsSuccessStatusCode)
+                throw new Exception($@"Error: {response.StatusCode}, {response.ReasonPhrase}");
+        }
+        
+        static async Task Main(string[] args) {
+            var program = new Program();
+
+            program.GetTodosAsync(null, null, null);
+            program.GetTodosAsync(null, null, false);
+            program.GetTodosAsync(1, null, true);
+            
+            program.AddTodoAsync(new Todo() {
+                completed = true,
+                id = 5,
+                title = "hello",
+                userId = 8
+            });
+            program.RemoveTodoAsync(5);
+        }
+    }
+}
---
+++ Lecture8/TodoWebAPI-WithoutBlazor/Todo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoWebAPI_WithoutBlazor {
+    public class Todo {
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
+        public int userId { get; set; }
+        public int id { get; set; }
+        [Required, MaxLength(128)]
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+}
