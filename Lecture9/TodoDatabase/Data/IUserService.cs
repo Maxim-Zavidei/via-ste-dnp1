572a28434fad03a254189577182057f25b6f3d90
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-11-09 11:49:09 +0100

    Solution for TodoDatabase exercise from lecture 9

---
+++ Lecture9/TodoDatabase/Data/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+using TodoDatabase.Models;
+
+namespace TodoDatabase.Data {
+    public interface IUserService {
+        Task<User> ValidateUser(string userName, string passWord);
+    }
+}
---
+++ Lecture9/TodoDatabase/Persistence/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoDatabase.Models;
+
+namespace TodoDatabase.Persistence {
+    public class TodoContext : DbContext {
+        
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlite(@"Data Source = D:\repos\via-ste-dnp1\Lecture9\TodoDatabase\TodoDatabase.db");
+        }
+    }
+}
